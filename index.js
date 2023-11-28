79240d0aadb0510e9cd8ec098394a0ae1b99237f
nymulislam <naymulislam241@gmail.com>
2023-11-29 05:50:50 +0600

    Added Update & Delete Route for User & Admin

--- index.js
+++ index.js
@@ -30,10 +30,66 @@ async function run() {
         await client.connect();
 
         const usersCollection = client.db('ilmMedDB').collection('allUsers')
+        const divisionsCollection = client.db('ilmMedDB').collection('divisions')
+        const districtsCollection = client.db('ilmMedDB').collection('districts')
+        const allTestsCollection = client.db('ilmMedDB').collection('allTests')
 
         // normal routes start
+        app.get("/divisions", async (req, res) => {
+            const result = await divisionsCollection.find().toArray();
+            res.send(result)
+        })
+        app.get("/districts", async (req, res) => {
+            const result = await districtsCollection.find().toArray();
+            res.send(result)
+        })
 
-        // ----------------------
+        // add a new test
+        app.post('/allTests', async (req, res) => {
+            const newTest = req.body;
+            const result = await allTestsCollection.insertOne(newTest);
+            res.send(result)
+        })
+
+        app.get('/allTests', async (req, res) => {
+            const result = await allTestsCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.get('/allTests/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await allTestsCollection.findOne(query);
+            res.send(result)
+        })
+
+         // update user info
+         app.put('/allTests/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const updateTest = req.body;
+            const info = {
+                $set: {
+                    name: updateTest.name,
+                    category: updateTest.category,
+                    slot: updateTest.slot,
+                    price: updateTest.price,
+                    date: updateTest.date,
+                    description: updateTest.description,
+                    testImage: updateTest.testImage,
+                }
+            }
+            const options = { upsert: true };
+            const result = await allTestsCollection.updateOne(query, info, options)
+            res.send(result)
+        })
+
+        app.delete('/allTests/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await allTestsCollection.deleteOne(query);
+            res.send(result)
+        })
 
         // normal routes end
 
@@ -94,7 +150,7 @@ async function run() {
             res.send({ admin });
         })
 
-        app.post("/users", async (req, res) => {
+        app.post("/users", verifyToken, async (req, res) => {
             const user = req.body;
             const query = { email: user.email }
             const existingUser = await usersCollection.findOne(query);
@@ -102,6 +158,65 @@ async function run() {
                 return res.send({ message: 'user already exists', insertedId: null })
             }
             const result = await usersCollection.insertOne(user);
+            res.send(result)
+        })
+
+
+        // for user info
+        app.get("/users/:id", verifyToken, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await usersCollection.findOne(query)
+            res.send(result)
+        })
+
+        // update user info
+        app.put('/users/:id', verifyToken, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const updateProfile = req.body;
+            const info = {
+                $set: {
+                    name: updateProfile.name,
+                    blood: updateProfile.blood,
+                    division: updateProfile.division,
+                    district: updateProfile.district,
+                }
+            }
+            const options = { upsert: true };
+            const result = await usersCollection.updateOne(query, info, options)
+            res.send(result)
+        })
+
+        // verify user
+        app.get('/users/status/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'Access unauthorized' })
+            }
+
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let status = false;
+            if (user) {
+                status = user?.status === 'active';
+            }
+            res.send({ active: status });
+        })
+
+        // user status handle
+        app.patch('/users/status/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            if (!id) {
+                return res.status(400).send({ message: 'User ID is required.' })
+            }
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    status: req.body.status || 'active'
+                }
+            };
+            const result = await usersCollection.updateOne(filter, updatedDoc);
             res.send(result)
         })
 
@@ -122,20 +237,15 @@ async function run() {
         })
 
 
-        // user status handle
-        app.patch('/users/status/:id', verifyToken, verifyAdmin, async (req, res) => {
-            const id = req.params.id;
-            if (!id) {
-                return res.status(400).send({ message: 'User ID is required.' })
+        // verify user profile
+        app.get('/users/userProfile/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'Access unauthorized' })
             }
-            const filter = { _id: new ObjectId(id) };
-            const updatedDoc = {
-                $set: {
-                    status: req.body.status || 'active'
-                }
-            };
-            const result = await usersCollection.updateOne(filter, updatedDoc);
-            res.send(result)
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            res.send(user);
         })
 
 
